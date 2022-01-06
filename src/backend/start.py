c1551a5043cd51bb26bad7773ce45fdce3ce37a6
bmnascimento <b@bmnascimento.com>
2022-01-06 04:29:37 -0300

    Convertendo porta para int

--- src/backend/start.py
+++ src/backend/start.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1 or sys.argv[1] == "server":
         port = 8000
         if len(sys.argv) == 3:
-            port = sys.argv[2]
+            port = int(sys.argv[2])
         uvicorn.run("start_fastapi:app", host='0.0.0.0', port=port, reload=True, debug=True, workers=3)
     elif sys.argv[1] == "test":
         pytest.main(["-v", "--junitxml=/tmp/reports/pytest.xml", "-x", "tests"])
